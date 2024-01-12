7ef9a76c5a60cb03ea940d1a4c621be0a5dbf19f
Aiman Ares <aaimanaress@gmail.com>
2024-01-12 09:46:46 +0800

    About screen full height screen

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,20 +6,22 @@ import { IconContext } from "react-icons/lib";
 
 function About() {
   return (
-    <div className="flex flex-col items-center py-12 mt-72 mb-72">
-      <h1 className="text-9xl text-center font-bold m-6 font-bebas">
-        AIMAN ARES
-      </h1>
-      <p className="text-gray-200 text-center w-11/12 md:w-2/3">
-        Recent IT graduate passionate about design with 6 years of creative
-        experience. Eager to transition into Front-End Development, I thrive on
-        continuous learning and adaptability. Let's connect and explore how I
-        can contribute to your team's success!
-      </p>
-      <div className="mt-12 flex flex-row">
-        <IconContext.Provider value={{ color: "#FFF", size: 40 }}>
-          <FaLinkedin /> <FaBehanceSquare /> <FaGithubSquare />
-        </IconContext.Provider>
+    <div className="flex h-screen items-center">
+      <div className="flex flex-col items-center py-12 ">
+        <h1 className="text-9xl text-center font-bold m-6 font-bebas">
+          AIMAN ARES
+        </h1>
+        <p className="text-gray-200 text-center w-11/12 md:w-2/3">
+          Recent IT graduate passionate about design with 6 years of creative
+          experience. Eager to transition into Front-End Development, I thrive
+          on continuous learning and adaptability. Let's connect and explore how
+          I can contribute to your team's success!
+        </p>
+        <div className="mt-12 flex flex-row">
+          <IconContext.Provider value={{ color: "#FFF", size: 40 }}>
+            <FaLinkedin /> <FaBehanceSquare /> <FaGithubSquare />
+          </IconContext.Provider>
+        </div>
       </div>
     </div>
   );
