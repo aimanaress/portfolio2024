c6211f3dae8ea57bdb18a1ef2f69834ef3a5253d
Aiman Ares <aaimanaress@gmail.com>
2024-01-13 09:32:02 +0800

    Add scroll animation

--- src/App.js
+++ src/App.js
@@ -5,19 +5,40 @@ import Skills from "./components/Skills";
 import Educations from "./components/Educations";
 import Career from "./components/Career";
 import Connect from "./components/Connect";
+import React, { useRef, useEffect } from "react";
 
 function App() {
+  const about = useRef(null);
+  const work = useRef(null);
+  const skills = useRef(null);
+  const educations = useRef(null);
+  const career = useRef(null);
+  const connect = useRef(null);
+
   return (
     <div className="flex flex-col">
+      {/* <NavBar /> */}
       <NavBar />
 
       <div className="container mx-auto w-10/12">
-        <About />
-        <Works />
-        <Skills />
-        <Educations />
-        <Career />
-        <Connect />
+        <div id="1" ref={about} className="element">
+          <About />
+        </div>
+        <div id="2" ref={work} className="element">
+          <Works />
+        </div>
+        <div id="3" ref={skills} className="element">
+          <Skills />
+        </div>
+        <div id="4" ref={educations} className="element">
+          <Educations />
+        </div>
+        <div id="5" ref={career} className="element">
+          <Career />
+        </div>
+        <div id="6" ref={connect} className="element">
+          <Connect />
+        </div>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,34 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import { Link, animateScroll as scroll, scrollSpy } from "react-scroll";
 
 function NavBar() {
   // Array containing navigation items
   const navItems = [
-    { id: 1, label: "About", href: "#", current: true },
-    { id: 2, label: "Works", href: "#", current: false },
-    { id: 3, label: "Skills", href: "#", current: false },
-    { id: 4, label: "Educations", href: "#", current: false },
-    { id: 5, label: "Career", href: "#", current: false },
-    { id: 6, label: "Connect", href: "#", current: false },
+    { id: 1, label: "About" },
+    { id: 2, label: "Works" },
+    { id: 3, label: "Skills" },
+    { id: 4, label: "Educations" },
+    { id: 5, label: "Career" },
+    { id: 6, label: "Connect" },
   ];
 
   return (
     <div className="invisible md:visible bg-black flex flex-col items-center p-3 text-white fixed w-full top-0 left-0">
-      {/* Desktop Navigation */}
       <ul className="flex">
         {navItems.map((item) => (
-          <li
+          <Link
             key={item.id}
+            activeClass="active"
+            to={item.id}
+            spy={true}
+            isDynamic={true}
+            offset={-50}
+            smooth="easeInOutQuint"
+            duration={1000}
             className="px-3 py-1 hover:bg-[#dfff06] rounded-md m-2 cursor-pointer duration-300 hover:text-black"
           >
             {item.label}
-          </li>
+          </Link>
         ))}
       </ul>
     </div>
